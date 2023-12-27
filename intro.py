3502ca5476c73f3512499bd4a7437494c54d9eb0
P-Drop <pedrorincon.tec@gmail.com>
2023-12-27 14:57:29 +0100

    mostrar eventos

--- intro.py
+++ intro.py
@@ -11,6 +11,7 @@ screen = pygame.display.set_mode(size)
 while True:
     # registrar eventos en la ventana
     for event in pygame.event.get():
+        print(event)
         if event.type == pygame.QUIT:
             sys.exit()
 
