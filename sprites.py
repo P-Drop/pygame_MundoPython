481a48fdde0b4f3b65be6b3097cbe8cacf167f9f
P-Drop <pedrorincon.tec@gmail.com>
2023-12-27 19:37:32 +0100

    balon recoge monedas

---
+++ sprites.py
@@ -0,0 +1,67 @@
+import pygame, random
+from arts.colors import Color
+
+class Moneda(pygame.sprite.Sprite):
+    def __init__(self) -> None:
+        super().__init__()
+        self.image = pygame.image.load('arts/elementos/coin.png').convert()
+        self.image.set_colorkey(Color.WHITE.value)
+        self.rect = self.image.get_rect()
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self) -> None:
+        super().__init__()
+        self.image = pygame.image.load('arts/elementos/balon.png').convert()
+        self.image.set_colorkey(Color.WHITE.value)
+        self.rect = self.image.get_rect()
+
+pygame.init()
+
+screen_size = (720, 720)
+screen = pygame.display.set_mode(screen_size)
+
+clock = pygame.time.Clock()
+
+pygame.mouse.set_visible(0)
+
+score = 0
+
+done = False
+
+background = pygame.image.load('arts/bg/bg_azul.png').convert()
+
+
+monedas_list = pygame.sprite.Group()
+all_sprite_list = pygame.sprite.Group()
+
+for i in range(50):
+    moneda = Moneda()
+    moneda.rect.x = random.randrange(720)
+    moneda.rect.y = random.randrange(720)
+    monedas_list.add(moneda)
+    all_sprite_list.add(moneda)
+
+player = Player()
+all_sprite_list.add(player)
+
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+
+    player.rect.x, player.rect.y = pygame.mouse.get_pos()
+
+    moneda_hit_list = pygame.sprite.spritecollide(player, monedas_list, True)
+    for moneda in moneda_hit_list:
+        score += 1
+        print(score)
+
+    screen.blit(background, [0,0])
+
+    all_sprite_list.draw(screen)
+
+    pygame.display.flip()
+    clock.tick(60)
+
+
+pygame.quit()
