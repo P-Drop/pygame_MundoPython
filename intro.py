517aee8ae1d36184d7e4fd5782d48e9891f4c785
P-Drop <pedrorincon.tec@gmail.com>
2023-12-27 15:13:25 +0100

    dibujando

--- intro.py
+++ intro.py
@@ -3,15 +3,47 @@ import pygame, sys
 # inicializar pygame
 pygame.init()
 
+# definir colores
+BLACK   =   (   0,   0,   0)
+WHITE   =   ( 255, 255, 255)
+GREEN   =   (   0, 255,   0)
+RED     =   ( 255,   0,   0)
+BLUE    =   (   0,   0, 255)
+
+
+
 # definir tamaño de ventana
 size = (800, 500)
 screen = pygame.display.set_mode(size)
 
 # crear bucle principal
 while True:
+    
     # registrar eventos en la ventana
     for event in pygame.event.get():
-        print(event)
+        # cerrar ventana al pulsar CERRAR
         if event.type == pygame.QUIT:
             sys.exit()
+
+    # color de fondo
+    screen.fill(WHITE)
+
+    """
+    ---- ZONA DE DIBUJO
+    """
+    # linea horizontal
+    pygame.draw.line(screen, GREEN, [0, 100], [100, 100], 5)
+    
+    # rectangulo
+    pygame.draw.rect(screen, BLACK, (100, 100, 80, 80))
+
+    # circulo
+    pygame.draw.circle(screen, RED, [300, 300], 30)
+
+    """
+    ---- ZONA DE DIBUJO 
+    """
+    
+    # actualizar pantalla
+    pygame.display.flip()
 
